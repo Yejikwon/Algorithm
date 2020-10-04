45c3c60134383d68e741892f8267fd0432883bec
Yancy <yeeji77@naver.com>
2020-10-04 22:55:25 +0900

    Create BOJ_11656_접미사배열.java

---
+++ BOJ/BOJ_11656_접미사배열.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.TreeMap;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		String str = bf.readLine();
+		TreeMap<String, String> map = new TreeMap<>();
+		
+		for(int i=0; i<str.length(); i++) {
+			String now = str.substring(i,str.length());
+			map.put(now, now);
+		}
+		
+		map.forEach((key, value)->System.out.println(key));
+	}
+}
